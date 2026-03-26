79bb50325d32418e6a6406813b30dba9daff44e4
Kaitlyn Chung <kch4018@cayuga-login1.localdomain>
2026-03-26 13:17:34 -0400

    Switched directory for gtf and input directory

--- run_qorts.sh
+++ run_qorts.sh
@@ -7,18 +7,15 @@
 #SBATCH --job-name=qorts
 #SBATCH --mem=100G
 
-bam_file=/athena/angsd/scratch/kch4018/angsd_project/pre_T/processed/SRR31204761.Aligned.sortedByCoord.out.bam 
-gtf_file=/athena/angsd/scratch/kch4018/angsd_project/index_files/genomic.gtf
-out_dir=/athena/angsd/scratch/kch4018/angsd_project/pre_T/processed/qc
-
+gtf_file=/athena/angsd/scratch/kch4018/angsd_project/genome_index/hg38.knownGene.gtf 
+out_dir=/athena/angsd/scratch/kch4018/angsd_project/processed/qc
+input_dir=/athena/cayuga_0019/scratch/kch4018/angsd_project/processed/*.bam
 jar_file=/athena/angsd/scratch/mef3005/share/envs/qorts/share/qorts-1.3.6-1/QoRTs.jar
-
-mkdir -p $out_dir
 
 conda activate qorts
 java -jar $jar_file \
 QC --stranded --generatePlots \
-$bam_file $gtf_file $out_dir
+$input_dir $gtf_file $out_dir
 
 java -jar $jar_file generateSamplePlots $out_dir \
     --makeSeparatePngs

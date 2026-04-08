aaaad9453108dc9f7f0467f627d6976b0e4db602
Kaitlyn Chung <kch4018@cayuga-login1.localdomain>
2026-04-08 11:30:06 -0400

    For QC on trimmed and original FASTQ files, not .bams

--- do_qc.sh
+++ do_qc.sh
@@ -3,9 +3,17 @@
 #SBATCH --partition=angsd_class
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
 #SBATCH --job-name=do_qc
-#SBATCH --mem=100G
+#SBATCH --mem=40G
 
-conda activate angsd
-fastqc *.bam --extract -t 12 -o "qc"
+conda activate multiqc
+
+DIRS=(
+  "/athena/angsd/scratch/kch4018/angsd_project/pre_T"
+  "/athena/angsd/scratch/kch4018/angsd_project/post_T"
+)
+
+for dir in "${DIRS[@]}"; do
+    echo "Running MultiQC on ${dir}"
+    multiqc "$dir" -o "$dir/multiqc"
+done

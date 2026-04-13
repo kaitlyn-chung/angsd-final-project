81e25cb9ae6901fecaef806d3af6075732c0d478
Kaitlyn Chung <kch4018@cayuga-login1.localdomain>
2026-04-13 00:10:43 -0400

    Included the correct directories and added time limit

--- rename_files.sh
+++ rename_files.sh
@@ -4,7 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=rename
-#SBATCH --mem=40G
+#SBATCH --mem=2G
+#SBATCH --time=00:10:00
+
+preT_dir="/athena/angsd/scratch/kch4018/angsd_project/pre_T"
+postT_dir="/athena/angsd/scratch/kch4018/angsd_project/on_T"
+table="/athena/angsd/scratch/kch4018/angsd_project/SRP542825.txt"
 
 DIRS=($preT_dir $postT_dir)
 

ad555b1d69ba76a43f48729d830eacdb7fca4521
Kaitlyn Chung <kch4018@cayuga-login1.localdomain>
2026-04-13 14:03:24 -0400

    Restructured alignment to use batch array

--- run_alignment.sh
+++ run_alignment.sh
@@ -3,37 +3,35 @@
 #SBATCH --partition=angsd_class
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --job-name=align_reads
+#SBATCH --cpus-per-task=12
+#SBATCH --array=1-16
+#SBATCH --job-name=alignment
 #SBATCH --mem=100G
 
 STAR_Dir="/athena/angsd/scratch/kch4018/angsd_project/genome_index"
 
-DIRS=(
-  "/athena/angsd/scratch/kch4018/angsd_project/pre_T"
-  "/athena/angsd/scratch/kch4018/angsd_project/post_T"
-)
+samples_list="/athena/angsd/scratch/kch4018/angsd_project/reads.txt"
+
+r1=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $samples_list)
+base="${r1%_1_val_1.fq.gz}"
+r2="${base}_2_val_2.fq.gz"
+
+sample="$(basename "$base")"
+outdir="/athena/angsd/scratch/kch4018/angsd_project/STAR_aligned"
+mkdir -p "$outdir"
+
 
 conda activate angsd
 
-for dir in "${DIRS[@]}"; do
-    for r1 in "$dir"/*_1_val_1.fq.gz; do
-    echo "DIR: $dir"
-    
-    # Base path without the suffix
-    base="${r1%_1_val_1.fq.gz}"
+if [[ ! -s "$r1" || ! -s "$r2" ]]; then
+    echo "[$(date)] Skipping sample '$sample': paired files missing or empty"
+    exit 0
+fi
 
-    # Mates and sample name
-    r2="${base}_2_val_2.fq.gz"
-    sample="$(basename "$base")"
-    out_prefix="${dir}/${sample}."
-
-    STAR --runMode alignReads \
-        --runThreadN 8 \
-        --genomeDir "$STAR_Dir" \
-        --readFilesIn "${r1}" "${r2}" \
-        --readFilesCommand zcat \
-        --outFileNamePrefix "$out_prefix" \
-        --outSAMtype BAM SortedByCoordinate
-    done
-done
+STAR --runMode alignReads \
+    --runThreadN 12 \
+    --genomeDir "$STAR_Dir" \
+    --readFilesIn "${r1}" "${r2}" \
+    --readFilesCommand zcat \
+    --outFileNamePrefix "$outdir/$sample." \
+    --outSAMtype BAM SortedByCoordinate

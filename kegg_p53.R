2a7bd23d19d0dba8f5d342b2dda45dde1d55fb0c
Kaitlyn Chung <kaitlyncutie326@gmail.com>
2026-04-16 09:20:04 -0400

    Initial commit.
    This is the script for KEGG pathway enrichment for the p53 pathway and analysis of which p53 genes were upregulated.

---
+++ kegg_p53.R
@@ -0,0 +1,58 @@
+library(DESeq2)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(KEGGREST)
+library(enrichplot)
+library(dplyr)
+
+df_results <- readRDS("df_results.rds")
+
+all_genes <- rownames(df_results)
+
+ids <- bitr(all_genes,
+            fromType = "SYMBOL",
+            toType   = "ENTREZID",
+            OrgDb    = hg38)
+
+gene_list_entrez <- gene_list[ids$SYMBOL]
+names(gene_list_entrez) <- ids$ENTREZID
+gene_list_entrez <- sort(gene_list_entrez, decreasing = TRUE)
+
+# KEGG GSEA
+kegg_gsea <- gseKEGG(
+  geneList  = gene_list_entrez,
+  organism  = "hsa",
+  minGSSize = 10,
+  pvalueCutoff = 1 # to look at ALL p53 signaling genes, not just what is significant
+)
+
+# Extract p53
+p53_gsea <- kegg_gsea@result %>%
+  dplyr::filter(ID == "hsa04115")
+p53_gsea[, c("NES", "pvalue", "p.adjust")]
+
+gseaplot2(
+  kegg_gsea,
+  geneSetID = "hsa04115",
+  title = "GSEA: p53 signaling pathway"
+)
+
+# pull out genes from rlog_dge
+pathway_info <- keggGet("hsa04115")
+gene_list <- pathway_info[[1]]$GENE
+gene_list <- gene_list[seq(2, length(gene_list), by = 2)]
+gene_list <- sub(";.*", "", gene_list)
+
+p53_genes_in_data <- intersect(
+  gene_list,
+  rownames(rlog_dge)
+)
+
+p53_genes_in_data  # the single gene identified
+
+df_results["CDKN1A",] # find p-value
+
+par(mfrow=c(1,3))
+plotCounts(dds_nav,gene=which.min(df_results$padj), main="MMP9 \nMost significant")
+plotCounts(dds_nav,gene = p53_genes_in_data, normalized=TRUE)
+plotCounts(dds_nav,gene=which.max(df_results$padj), main="RAC3 \nLeast significant")
